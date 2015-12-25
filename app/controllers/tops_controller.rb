afd146866a0b67e1f3b989c2c25b7674ccb0163e
nokkii <nokkii.03@gmail.com>
2015-12-25 10:49:14 +0900

    名前リストを登録できるようにした

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -2,5 +2,15 @@ class TopsController < ApplicationController
   def index
     @current_client_list = CurrentMacs.all
     @named_list = NamedList.all
+    @client = NamedList.new
+  end
+  
+  def create
+    NamedList.create do |client|
+      client.name = params[:named_list][:name]
+      client.ip_address = params[:named_list][:ip_address]
+      client.mac_address = params[:named_list][:mac_address]
+    end
+    redirect_to :tops
   end
 end
