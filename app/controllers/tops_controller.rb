792f5b75e61e714cb31635019afd71fe924082ec
nokkii <nokkii.03@gmail.com>
2015-12-25 10:30:18 +0900

    登録名前リストを表示

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,5 +1,6 @@
 class TopsController < ApplicationController
   def index
     @current_client_list = CurrentMacs.all
+    @named_list = NamedList.all
   end
 end
---
+++ app/models/named_list.rb
@@ -0,0 +1,2 @@
+class NamedList < ActiveRecord::Base
+end
